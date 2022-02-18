385324ac1830cee18568bb9d3930436bc39e3f0a
Ryo-Bam <euratom1958.1.1.r@icloud.com>
2022-02-18 16:19:20 +0900

    api修正12回目

--- resources/js/src/Products/Memos/MemoQuesModal.jsx
+++ resources/js/src/Products/Memos/MemoQuesModal.jsx
@@ -69,6 +69,7 @@ const MemoQuesModal = ({ show, setShow, store_id, name, category }) => {
 
     //メモの情報をデータベースに送信
     const createMemo = async (e) => {
+        e.preventDefault();
         if (formData == '') {
             return;
         }
